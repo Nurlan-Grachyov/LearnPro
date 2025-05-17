1401014ac2c1e49b949f7da08c0bd459ebc80eb0
Nurlan-Grachyov <nurlan.grachyov@mail.ru>
2025-05-17 23:00:34 +0400

    the problem with launching wia a terminal

--- config/settings.py
+++ config/settings.py
@@ -90,9 +90,9 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("POSTGRES_DB"),
-        "USER": "superuser",
+        "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": "db",
+        "HOST": os.getenv("POSTGRES_HOST"),
         "PORT": os.getenv("PORT"),
     }
 }
